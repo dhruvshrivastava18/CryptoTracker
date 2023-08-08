68c0d2b3f9de62415753ef6d62171e24c9225dde
dhruvshrivastava18 <dhruvshrivastava18@gmail.com>
2023-08-08 15:26:23 +0530

    Updated UI

--- SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
+++ SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct SettingsView: View {
     
-    let defaultURL = URL(string: "https://www.google.com")!
-    let youtubeURL = URL(string: "https://www.youtube.com/c/swiftfulthinking")!
-    let coffeeURL = URL(string: "https://www.buymeacoffee.com/nicksarno")!
+
     let coingeckoURL = URL(string: "https://www.coingecko.com")!
-    let personalURL = URL(string: "https://www.nicksarno.com")!
+
     
     var body: some View {
         NavigationView {
@@ -24,14 +22,12 @@ struct SettingsView: View {
                 
                 // content
                 List {
-                    swiftfulThinkingSection
-                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    
                     coinGeckoSection
                         .listRowBackground(Color.theme.background.opacity(0.5))
                     developerSection
                         .listRowBackground(Color.theme.background.opacity(0.5))
-                    applicationSection
-                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    
                 }
             }
             .font(.headline)
@@ -55,23 +51,7 @@ struct SettingsView_Previews: PreviewProvider {
 
 extension SettingsView {
     
-    private var swiftfulThinkingSection: some View {
-        Section(header: Text("Swiftful Thinking")) {
-            VStack(alignment: .leading) {
-                Image("logo")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                Text("This app was made by following a @SwiftfulThinking course on YouTube. It uses MVVM Architecture, Combine, and CoreData!")
-                    .font(.callout)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.theme.accent)
-            }
-            .padding(.vertical)
-            Link("Subscribe on YouTube 🥳", destination: youtubeURL)
-            Link("Support his coffee addiction ☕️", destination: coffeeURL)
-        }
-    }
+    
     
     private var coinGeckoSection: some View {
         Section(header: Text("CoinGecko")) {
@@ -104,18 +84,7 @@ extension SettingsView {
                     .foregroundColor(Color.theme.accent)
             }
             .padding(.vertical)
-            Link("Visit Website 🤙", destination: personalURL)
+            
         }
     }
-    
-    private var applicationSection: some View {
-        Section(header: Text("Application")) {
-            Link("Terms of Service", destination: defaultURL)
-            Link("Privacy Policy", destination: defaultURL)
-            Link("Company Website", destination: defaultURL)
-            Link("Learn More", destination: defaultURL)
-        }
-    }
-    
-    
 }
